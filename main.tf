38eb9f90db51c55a59bc568c81b1561a01e5dbf2
ARUP KUMAR DASH <arupkumard31@gmail.com>
2026-06-27 21:44:38 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "my_ec2" {
+  ami           = "ami-08f44e8eca9095668"   # Amazon Linux 2023 (us-east-1)
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "terraform-ec2-arup"
+  }
+}
