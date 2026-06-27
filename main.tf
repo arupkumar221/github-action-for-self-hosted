f67a6b095b6b4dadbbd00b2f4171f79abb5d4e30
ARUP KUMAR DASH <arupkumard31@gmail.com>
2026-06-27 22:02:52 +0530

    Configure AWS provider in main.tf
    Add Terraform provider configuration for AWS.

--- main.tf
+++ main.tf
@@ -1,5 +1,14 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "6.52.0"
+    }
+  }
+}
+
 provider "aws" {
-  region = "us-east-1"
+  # Configuration options
 }
 
 resource "aws_instance" "my_ec2" {
